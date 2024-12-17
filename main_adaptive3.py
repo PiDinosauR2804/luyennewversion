26e21a6cd7ef16dafb362eb92c96042c878d96f6
PiDinosaur2804 <ngodinhluyennht@gmail.com>
2024-12-18 00:18:50 +0700

    100 75 3600

--- main_adaptive3.py
+++ main_adaptive3.py
@@ -27,7 +27,7 @@ global BEST
 global start_time
 
 # Set up chỉ số -------------------------------------------------------------------
-ITE = 5
+ITE = 3
 epsilon = (-1) * 0.00001
 # 15:   120,    20:    150
 # BREAKLOOP = Data.number_of_cities * 
